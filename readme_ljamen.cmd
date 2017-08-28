4ce111577c95c4bb42c11af9b6273a12dbd4935d
ljamen <31409174+ljamen@users.noreply.github.com>
2017-08-28 15:45:15 -0400

    Update and rename readme_ljamen to readme_ljamen.cmd

--- readme_ljamen.cmd
+++ readme_ljamen.cmd
@@ -1,8 +1,8 @@
-Testing Markdown
+# Testing Markdown
 [Yahoo](https://www.yahoo.com)
 
 [Yahoo Homepage](https://www.yahoo.com "Yahoo's Homepage")
-# H1 This is an H!
+# H1 This is an H1
 ## H2 This is an H2
 ### Header3 This is an H#
 This is **bolded** and this is __italics__
